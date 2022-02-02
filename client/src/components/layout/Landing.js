2bbea5a97743274af4dfc23be8cda94ae31e6ac0
Andreas Olsson <andreas.o.olsson@outlook.com>
2022-02-02 14:02:40 +0100

    Initial commit

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -16,7 +16,7 @@ const Landing = ({ isAuthenticated }) => {
       <section className='landing dark-overlay'>
         <div>
           <div className='landing-content'>
-            <h1 className='x-large light-font'>Welcome to BaitScore</h1>
+            <h1 className='x-large light-font'>Välkommen till ABB-Akuten</h1>
             <p className='lead light-font'>
               BaitScore is a social network that connects fishermen worldwide
               and lets users share their favorite fishing stories, techniques
